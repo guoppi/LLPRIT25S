42893cf86704ebe2496b11d108a4ebd7b39e1926
Jatu <860093+guoppi@users.noreply.github.com>
2025-10-24 01:36:08 +0300

    Update Exz10.cs

--- Exz10.cs
+++ Exz10.cs
@@ -1,121 +1,115 @@
-using System;
-namespace Programs
+using System; // Tuodaan käyttöön System-kirjasto, joka sisältää esim. Console-luokan tulostusta varten.
+
+class Program // Luodaan luokka nimeltä Program. Kaikki koodi on tämän sisällä.
 {
-    public class Solution
+    static void Main() // Ohjelman aloituspaikka. Tämä suoritetaan ensimmäisenä kun ohjelma käynnistyy.
     {
-        //IMPORTANT NOTE: you should declare ALL subroutines here in VPL (not in Visual Studio) as public non-static as
-        //SO -- > Replace the word static with word public to make the test program compile
-        //Tulee selitys myöhemmin
-        public void ExampleSubroutine()
-        {
-        //No need to remove this subroutine
         // Testataan OSquare4 viisi kertaa
-        int count = 0;
-        while (count < 5)
+        int count = 0; // Alustetaan laskuri count arvolla 0.
+        while (count < 5) // Toistetaan niin kauan kuin count on alle 5.
         {
-            OSquare4();
-            Console.WriteLine(); // Tyhjä rivi erottamaan neliöt
-            count++;
+            OSquare4(); // Kutsutaan aliohjelmaa, joka piirtää 4x4 neliön kirjaimilla 'o'.
+            Console.WriteLine(); // Tulostetaan tyhjä rivi neliöiden väliin.
+            count++; // Lisätään countiin yksi, jotta silmukka etenee.
         }
 
         // Testataan OSquareN arvoilla 1–5
-        int n = 1;
-        while (n <= 5)
+        int n = 1; // Alustetaan muuttuja n arvolla 1.
+        while (n <= 5) // Toistetaan niin kauan kuin n on enintään 5.
         {
-            OSquareN(n);
-            Console.WriteLine(); // Tyhjä rivi erottamaan neliöt
-            n++;
+            OSquareN(n); // Kutsutaan aliohjelmaa, joka piirtää n x n neliön kirjaimilla 'o'.
+            Console.WriteLine(); // Tulostetaan tyhjä rivi neliöiden väliin.
+            n++; // Lisätään n:ään yksi, jotta silmukka etenee.
         }
 
         // Testataan XSquareN eri kokoisilla ja eri merkeillä
-        XSquareN(3, '*');
-        Console.WriteLine();
-        XSquareN(5, '#');
-        Console.WriteLine();
-        XSquareN(2, '+');
-        Console.WriteLine();
+        XSquareN(3, '*'); // Piirretään 3x3 neliö tähdillä '*'.
+        Console.WriteLine(); // Tyhjä rivi.
+        XSquareN(5, '#'); // Piirretään 5x5 neliö risuilla '#'.
+        Console.WriteLine(); // Tyhjä rivi.
+        XSquareN(2, '+'); // Piirretään 2x2 neliö plussilla '+'.
+        Console.WriteLine(); // Tyhjä rivi.
 
         // Testataan GetXSquareN ja tulostetaan palautettu merkkijono
-        string square = GetXSquareN(4, '@');
-        Console.WriteLine(square);
+        string square = GetXSquareN(4, '@'); // Kutsutaan aliohjelmaa, joka palauttaa 4x4 neliön @-merkeillä merkkijonona.
+        Console.WriteLine(square); // Tulostetaan palautettu merkkijono.
 
         // Testataan OutputSum
-        OutputSum(3.5, 4.2);
-        OutputSum(10.1, 5.9);
+        OutputSum(3.5, 4.2); // Lasketaan 3.5 + 4.2 ja tulostetaan tulos.
+        OutputSum(10.1, 5.9); // Lasketaan 10.1 + 5.9 ja tulostetaan tulos.
 
         // Testataan Sum ja tulostetaan tulos
-        double result1 = Sum(2.5, 7.5);
-        Console.WriteLine("Summa: " + result1);
+        double result1 = Sum(2.5, 7.5); // Lasketaan 2.5 + 7.5 ja tallennetaan tulos muuttujaan.
+        Console.WriteLine("Summa: " + result1); // Tulostetaan laskettu summa.
 
-        double result2 = Sum(100.25, 200.75);
-        Console.WriteLine("Summa: " + result2);
-        }
+        double result2 = Sum(100.25, 200.75); // Lasketaan 100.25 + 200.75 ja tallennetaan tulos.
+        Console.WriteLine("Summa: " + result2); // Tulostetaan laskettu summa.
+    }
 
-//NOTE: DO NOT ERASE ANYTHING ABOVE THIS LINE---------------------------------
-        
-        
-        //Enter your subroutines here, just as is, on by one and run the test
-        public void OSquare4()
+    // Aliohjelma, joka piirtää 4x4 neliön kirjaimilla 'o'.
+    static void OSquare4()
+    {
+        for (int i = 0; i < 4; i++) // Toistetaan 4 riviä.
         {
-            for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++) // Jokaisella rivillä tulostetaan 4 'o'-merkkiä.
             {
-                for (int j = 0; j < 4; j++)
-                {
-                    Console.Write("o");
-                }
-                Console.WriteLine();
+                Console.Write("o"); // Tulostetaan yksi 'o'.
             }
+            Console.WriteLine(); // Rivin vaihto.
         }
+    }
 
-        public void OSquareN(int n)
+    // Aliohjelma, joka piirtää n x n neliön kirjaimilla 'o'.
+    static void OSquareN(int n)
+    {
+        for (int i = 0; i < n; i++) // Toistetaan n riviä.
         {
-            for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) // Jokaisella rivillä tulostetaan n 'o'-merkkiä.
             {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write("o");
-                }
-                Console.WriteLine();
+                Console.Write("o"); // Tulostetaan yksi 'o'.
             }
+            Console.WriteLine(); // Rivin vaihto.
         }
+    }
 
-        public void XSquareN(int n, char merkki)
+    // Aliohjelma, joka piirtää n x n neliön valitulla merkillä.
+    static void XSquareN(int n, char merkki)
+    {
+        for (int i = 0; i < n; i++) // Toistetaan n riviä.
         {
-            for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) // Jokaisella rivillä tulostetaan n merkkiä.
             {
-                for (int j = 0; j < n; j++)
-                {
-                Console.Write(merkki);
-                }
-                Console.WriteLine();
+                Console.Write(merkki); // Tulostetaan valittu merkki.
             }
+            Console.WriteLine(); // Rivin vaihto.
         }
+    }
 
-        public string GetXSquareN(int n, char merkki)
+    // Aliohjelma, joka palauttaa n x n neliön merkkijonona.
+    static string GetXSquareN(int n, char merkki)
+    {
+        string tulos = ""; // Alustetaan tyhjä merkkijono.
+        for (int i = 0; i < n; i++) // Toistetaan n riviä.
         {
-            string tulos = "";
-            for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) // Jokaisella rivillä lisätään n merkkiä.
             {
-                for (int j = 0; j < n; j++)
-                {
-                    tulos += merkki;
-                }
-                tulos += "\n";
+                tulos += merkki; // Lisätään merkki merkkijonoon.
             }
-            return tulos;
+            tulos += "\n"; // Lisätään rivinvaihto.
         }
+        return tulos; // Palautetaan valmis merkkijono.
+    }
 
-        public void OutputSum(double a, double b)
-        {
-            double summa = a + b;
-            Console.WriteLine("Summa: " + summa);
-        }
+    // Aliohjelma, joka laskee kahden luvun summan ja tulostaa sen.
+    static void OutputSum(double a, double b)
+    {
+        double summa = a + b; // Lasketaan summa.
+        Console.WriteLine("Summa: " + summa); // Tulostetaan summa.
+    }
 
-        public double Sum(double a, double b)
-        {
-            return a + b;
-        }
-        
-//NOTE: DO NOT ERASE ANYTHING BELOW THIS LINE---------------------------------        
+    // Aliohjelma, joka laskee kahden luvun summan ja palauttaa sen.
+    static double Sum(double a, double b)
+    {
+        return a + b; // Palautetaan laskettu summa.
     }
 }
