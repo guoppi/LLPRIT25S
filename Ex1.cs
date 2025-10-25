71bc74c7d79e77567d5ab5c931281bbe6315090d
Jatu <860093+guoppi@users.noreply.github.com>
2025-10-25 11:07:24 +0300

    Create Ex1.cs

---
+++ Ex1.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tehtävä 1
+            
+            Console.WriteLine("Meow Owoh");
+            Console.WriteLine("");
+            
+            // Tehtävä 2
+            
+            Console.WriteLine("Jaturapit");
+            Console.WriteLine("Orasan");
+            Console.WriteLine("");
+            
+             // Tehtävä 3
+            
+            Console.WriteLine("   o");
+            Console.WriteLine("  ooo");
+            Console.WriteLine(" o   o");
+            Console.WriteLine("o     o");
+            Console.WriteLine(" o   o");
+            Console.WriteLine("  ooo");
+            Console.WriteLine("   o");
+            Console.WriteLine("");
+            
+            // Tehtävä 4
+            
+            Console.Write("   o\n");
+            Console.Write("  ooo\n");
+            Console.Write(" o   o\n");
+            Console.Write("o     o\n");
+            Console.Write(" o   o\n");
+            Console.Write("  ooo\n");
+            Console.Write("   o\n");
+            Console.WriteLine("");
+
+            // Tehtävä 5
+
+            Console.Write( 
+                "   o\n" +
+                "  ooo\n" +
+                " o   o\n" +
+                "o     o\n" +
+                " o   o\n" +
+                "  ooo\n" +
+                "   o\n"
+            );
+            Console.WriteLine("");
+
+            // Tehtävä 6
+            
+            string nimi = "Jatu Orasan";
+            int ika = 30;
+
+            Console.WriteLine(nimi + " " + ika + " v");
+
+
+            //Tehtävä 7 tämä on rivikommentti
+
+            /*
+            Tämä on usean rivin kommentti.
+            Tässä voi kirjoittaa useita rivejä.
+            */
+
+        }
+    }
+}
