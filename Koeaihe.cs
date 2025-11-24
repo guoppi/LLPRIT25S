4e7dc2aa24c813987c7f18ec095e67780cce5311
Jatu <860093+guoppi@users.noreply.github.com>
2025-11-24 12:34:03 +0200

    Update Koeaihe.cs

--- Koeaihe.cs
+++ Koeaihe.cs
@@ -6,7 +6,15 @@ namespace Programs
     {
     /* KOEAIHEET
     TYYPPIMUUNNOS
-    EI POIKKEUKSIA
+    EI POIKKEUKSIA OSIOT ELI TEHTÄVÄ 12
+
+    MUISTISÄÄNTÖJÄ
+    
+            i++;                // i = i + 1; SOPII FOR LOOPPIIN ESIM (1-10), i-- TAAS ON KÄÄNTEINEN ESIM HALUTAAN ISOMMASTA PIENEMPÄÄN (10-1)
+            i++; j += i;        // i = i + 1; j = j + i;
+            k *= i * j; k++;    // k = k * i * j; k = k + 1; 
+            j--; i += j + k;    // j = j - 1; i = i + (j + k); 
+            ++j; k -= j;        // j = j + 1; k = k - j;
     
     */
     }
