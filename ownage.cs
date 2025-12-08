28f0653584ac525b8049606a4c55e488413197e6
Jatu <860093+guoppi@users.noreply.github.com>
2025-12-08 14:14:28 +0200

    ownage fyra

---
+++ ownage.cs
@@ -0,0 +1,5 @@
+Kuukausikorko prosentteina => tyyppi r_prosentti = R / 12
+Kuukausikorko euroina => tyyppi r = (r_prosentti / 100) * C
+Pääoman lyhennys kuukaudessa => tyyppi C / P
+Kuukausierä yhteensä => tyyppi summa = M + r
+Palautetaan summa => return summa
