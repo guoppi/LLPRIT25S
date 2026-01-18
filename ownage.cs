595dec9ad47adb21ce6cbb7295d8510bdfaea621
Jatu <860093+guoppi@users.noreply.github.com>
2026-01-18 15:47:33 +0200

    Update ownage.cs

--- ownage.cs
+++ ownage.cs
@@ -1,12 +1,1 @@
-two
-2x for loop, muista käänteinen järjestys eli int i = taulukko -a; i>=0; i--
 
-tre
-switch case or if else
-
-fyra
-Kuukausikorko prosentteina => tyyppi r_prosentti = R / 12
-Kuukausikorko euroina => tyyppi r = (r_prosentti / 100) * C
-Pääoman lyhennys kuukaudessa => tyyppi C / P
-Kuukausierä yhteensä => tyyppi summa = M + r
-Palautetaan summa => return summa
